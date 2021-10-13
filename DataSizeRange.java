31e56024fd4e740a5b534a734d26a704029985aa
Naren <naren.p.2018.cse@ritchennai.edu.in>
2021-10-13 21:24:16 +0530

    till variables

---
+++ DataSizeRange.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+
+public class DataSizeRange {
+    public static void main(String[]args)
+    {
+        System.out.println("Integer Max: "+Integer.MAX_VALUE);
+        System.out.println("Integer Min: "+Integer.MIN_VALUE);
+        System.out.println("Integer Bytes: "+Integer.BYTES);
+
+        System.out.println("Float Max: "+Float.MAX_VALUE);
+        System.out.println("Float Min: "+Float.MIN_VALUE);
+        System.out.println("Float Bytes: "+Float.BYTES);
+    }    
+}
