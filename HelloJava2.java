00570ab19737dd1aa83072c4d3271186e6d208b3
Naren <naren.p.2018.cse@ritchennai.edu.in>
2022-01-06 08:29:33 +0530

    Jetbrains config

---
+++ HelloJava2.java
@@ -0,0 +1,5 @@
+public class HelloJava2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World from JetBrains IDE - IntelliJ IDE");
+    }
+}
---
+++ out/production/Java-SE/README.md
@@ -0,0 +1,1 @@
+Java SE Course by Abdul Bari
