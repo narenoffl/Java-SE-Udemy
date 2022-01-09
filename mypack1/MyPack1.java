63a54eb6e92311c653a55c494d176c0a54bf9e07
Naren <naren.p.2018.cse@ritchennai.edu.in>
2022-01-09 11:49:49 +0530

    Packages-Access Modifiers

---
+++ mypack1/MyPack1.java
@@ -0,0 +1,33 @@
+package mypack1;
+
+class Demo1{
+    int a=10;
+    public int b=20;
+    protected int c=20;
+    public int d=40;
+
+    public void display(){
+        System.out.println(a+b+c+d);
+    }
+}
+
+class Demo3 extends Demo1{
+    Demo1 d = new Demo1();
+    public void show(){
+        System.out.println(d.a+d.b+d.c+d.d);
+    }
+}
+
+class Demo4 extends Demo1{
+    public void show(){
+        System.out.println(a+b+c+d);
+    }
+}
+
+public class MyPack1 {
+    public static void main(String[] args) {
+        Demo1 d1 = new Demo1();
+        d1.display();
+        System.out.println(d1.a+d1.b+d1.c+d1.d);
+    }
+}
