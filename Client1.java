f46e776ff02929b11d3a8aa7a0e8d4ce2d3f6049
Naren <naren.p.2018.cse@ritchennai.edu.in>
2022-01-18 15:04:31 +0530

    Network Programming

---
+++ Client1.java
@@ -0,0 +1,147 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+// Client class
+public class Client1 {
+
+    // driver code
+    public static void main(String[] args) {
+        // establish a connection by providing host and port
+        // number
+        try (Socket socket = new Socket("localhost", 1234)) {
+
+            // writing to server
+            PrintWriter out = new PrintWriter(
+                    socket.getOutputStream(), true);
+
+            // reading from server
+            BufferedReader in
+                    = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+
+            // object of scanner class
+            Scanner sc = new Scanner(System.in);
+            String line = null;
+
+            while (!"exit".equalsIgnoreCase(line)) {
+
+                // reading from user
+                line = sc.nextLine();
+
+                // sending the user input to server
+                out.println(line);
+                out.flush();
+
+                // displaying server reply
+                System.out.println("Server replied "
+                        + in.readLine());
+            }
+
+            // closing the scanner object
+            sc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+// Server class
+class Server {
+    public static void main(String[] args) {
+        ServerSocket server = null;
+
+        try {
+
+            // server is listening on port 1234
+            server = new ServerSocket(1234);
+            server.setReuseAddress(true);
+
+            // running infinite loop for getting
+            // client request
+            while (true) {
+
+                // socket object to receive incoming client
+                // requests
+                Socket client = server.accept();
+
+                // Displaying that new client is connected
+                // to server
+                System.out.println("New client connected"
+                        + client.getInetAddress()
+                        .getHostAddress());
+
+                // create a new thread object
+                ClientHandler clientSock
+                        = new ClientHandler(client);
+
+                // This thread will handle the client
+                // separately
+                new Thread(clientSock).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // ClientHandler class
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+
+        // Constructor
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        public void run() {
+            PrintWriter out = null;
+            BufferedReader in = null;
+            try {
+
+                // get the outputstream of client
+                out = new PrintWriter(
+                        clientSocket.getOutputStream(), true);
+
+                // get the inputstream of client
+                in = new BufferedReader(
+                        new InputStreamReader(
+                                clientSocket.getInputStream()));
+
+                String line;
+                while ((line = in.readLine()) != null) {
+
+                    // writing the received message from
+                    // client
+                    System.out.printf(
+                            " Sent from the client: %s\n",
+                            line);
+                    out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                    if (in != null) {
+                        in.close();
+                        clientSocket.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
---
+++ DatagramClient1.java
@@ -0,0 +1,41 @@
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
+public class DatagramClient1 {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket ds = new DatagramSocket(2001);
+        String msg = "Hello World";
+        DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.length(), InetAddress.getByName("localhost"), 2000);
+
+        ds.send(dp);
+
+        byte b[] = new byte[1024];
+        dp = new DatagramPacket(b, 1024);
+        ds.receive(dp);
+
+        msg = new String(dp.getData()).trim();
+        System.out.println("From Server " + msg);
+
+        ds.close();
+    }
+}
+
+class DatagramServer1 {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket ds = new DatagramSocket(2000);
+        byte b[] = new byte[1024];
+        DatagramPacket dp = new DatagramPacket(b, 1024);
+        ds.receive(dp);
+
+        String msg = new String(dp.getData()).trim();
+        System.out.println("From Server " + msg);
+        StringBuilder sb = new StringBuilder(msg);
+        sb.reverse();
+        msg = sb.toString();
+
+        dp = new DatagramPacket(msg.getBytes(), msg.length(), InetAddress.getByName("localhost"), 2001);
+
+        ds.send(dp);
+        ds.close();
+    }
+}
